5ff13d85bafa16fd76a948836fedf55866ca83e8
Sana Anwar <sfa33@njit.edu>
2026-01-27 20:40:55 -0500

    hex2dec and dec2hex conversion

---
+++ convert.py
@@ -0,0 +1,28 @@
+convert = int(input("type 1 to convert from decimal to hexadecimal or 2 to convert from hexadecimal to decimal: "))
+conversionKey = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+if (convert == 1): # decimal to hexadecimal
+    hexResult = ''
+    dec = int(input("enter decimal number: "))
+    if (dec == 0):
+        hexResult = '0'
+    else:
+        while (dec > 0):
+            remainder = dec % 16
+            hexResult = conversionKey[remainder] + hexResult
+            dec = dec // 16
+    print("hexadecimal number: " + hexResult)
+
+elif (convert == 2):
+    hex = input("enter hexadecimal number: ")
+    decResult = 0
+    hexLength = len(hex)
+    for char in hex:
+        digit = conversionKey.index(char.upper())
+        decResult += digit * (16 ** (hexLength-1))
+        hexLength -= 1
+    
+    print("decimal number: " + str(decResult))
+    
+else:
+    print("invalid input")
